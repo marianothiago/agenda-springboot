daa606485db746e2924f0682ba9801c7fef810ac
Thiago Mariano <mariano.thiago@gmail.com>
2025-10-28 21:53:05 -0300

    Remoção de comentário

--- agenda/src/main/java/br/unifan/agenda/controller/exceptions/TokenInvalido.java
+++ agenda/src/main/java/br/unifan/agenda/controller/exceptions/TokenInvalido.java
@@ -4,7 +4,7 @@ package br.unifan.agenda.controller.exceptions;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED) // 401 Unauthorized
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class TokenInvalido extends RuntimeException{
     public TokenInvalido(){
         super(String.format("Token de acesso inválido"));
